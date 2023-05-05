0c1d4e0c64ad91e23e21b6eb6a4c3d05ff202388
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-05 10:26:42 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,13 @@
+"""You are counting points for a football game, given the
+amount of touchdowns, after points, and field goals find the
+final points for the team and return that value."""
+
+def final_score(touchdowns, after_points, two_point_con, field_goals):
+    touchdowns = touchdowns * 6
+    after_points = after_points
+    two_point_con = two_point_con * 2
+    field_goals = field_goals * 3
+    total_points = touchdowns + after_points + two_point_con + field_goals
+    return total_points
+
+print(final_score(1,1,1,1,))
